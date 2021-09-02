f4612917dec134fa40f0b098c16e43fdf091c5d2
PryosCode <info@pryoscode.net>
2021-09-02 21:58:44 +0200

    Update pacman clean

--- src/clean.rs
+++ src/clean.rs
@@ -1,4 +1,16 @@
+use std::process::Command;
+
 pub fn init() {
+    let output = Command::new("pacman").arg("-Qtdq").output().unwrap();
+    let lines = String::from_utf8(output.stdout).unwrap();
+    let packages: Vec<&str> = lines.lines().collect();
+    if !packages.is_empty() {
+        let status = Command::new("sudo").arg("pacman").args(["-Rns", "--noconfirm"]).args(packages).status().unwrap();
+        if !status.success() {
+            return;
+        }
+    }
+
     let folder = dirs::cache_dir().unwrap().join("toru");
     if folder.exists() {
         rm_rf::remove(folder).unwrap();
--- src/remove.rs
+++ src/remove.rs
@@ -4,5 +4,5 @@ use clap::{ArgMatches};
 pub fn init(matches: ArgMatches) {
     let args: Vec<&str> = matches.subcommand_matches("remove").unwrap().values_of("PACKAGES").unwrap().collect();
     let packages: Vec<String> = args.iter().map(|p| p.to_lowercase()).collect();
-    Command::new("sudo").arg("pacman").arg("-Rns").args(packages).status().unwrap();
+    Command::new("sudo").arg("pacman").args(["-Rns", "--noconfirm"]).args(packages).status().unwrap();
 }
